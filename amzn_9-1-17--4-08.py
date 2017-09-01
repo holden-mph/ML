7bafff4390294c4cab855677fc39d2a5e20c38aa
holden-mph <holden.mph@gmail.com>
2017-09-01 16:27:03 +0100

    Update amzn_9-1-17--4-08.py

--- amzn_9-1-17--4-08.py
+++ amzn_9-1-17--4-08.py
@@ -103,50 +103,49 @@ def scrape(queries):
 
 ################################ MAIN ################################
     
-#for t_idx in range(len(sheetnames)):
-t_idx = 2
-print('Scraping Amazon.{}'.format(territory_domains[t_idx]))
+for t_idx in range(len(sheetnames)):
+    print('Scraping Amazon.{}'.format(territory_domains[t_idx]))
 
-df = df_list[t_idx]
-df = df.iloc[0:15,:]
+    df = df_list[t_idx]
+    df = df.iloc[0:15,:]
 
-df['Consolidated Release Date (Text)'] = df['Consolidated Release Date (Text)'].apply(getReleaseYear)
-df['Local Title Name'] = df['Local Title Name'].apply(editTitleNames)
+    df['Consolidated Release Date (Text)'] = df['Consolidated Release Date (Text)'].apply(getReleaseYear)
+    df['Local Title Name'] = df['Local Title Name'].apply(editTitleNames)
 
-territory = sheetnames[t_idx]
-title_names = df.iloc[:,0]
+    territory = sheetnames[t_idx]
+    title_names = df.iloc[:,0]
 
-placeholder = np.zeros((title_names.shape[0], 4))
+    placeholder = np.zeros((title_names.shape[0], 4))
 
-sd_vod = placeholder[:,0].reshape(title_names.shape[0],1)
-hd_vod = placeholder[:,1].reshape(title_names.shape[0],1)
-sd_est = placeholder[:,2].reshape(title_names.shape[0],1)
-hd_est = placeholder[:,3].reshape(title_names.shape[0],1)
+    sd_vod = placeholder[:,0].reshape(title_names.shape[0],1)
+    hd_vod = placeholder[:,1].reshape(title_names.shape[0],1)
+    sd_est = placeholder[:,2].reshape(title_names.shape[0],1)
+    hd_est = placeholder[:,3].reshape(title_names.shape[0],1)
 
-queries = [url_bases[t_idx] + title + ' Amazon Video' for title in title_names]
+    queries = [url_bases[t_idx] + title + ' Amazon Video' for title in title_names]
 
-pdp_list = []
+    pdp_list = []
 
-for i in range(len(queries)):
-    try:
-        scrape(queries)          
-    except:
-        queries[i] = url_bases[t_idx] + title_names[i] + ' ' + df.iloc[i,3] + ' Amazon Video'
+    for i in range(len(queries)):
         try:
-            print('QUERY REMADE after reaching except (didnt work so trying year)')
-            scrape(queries)
+            scrape(queries)          
         except:
-            pdp_list.append('Error')
+            queries[i] = url_bases[t_idx] + title_names[i] + ' ' + df.iloc[i,3] + ' Amazon Video'
+            try:
+                print('QUERY REMADE after reaching except (didnt work so trying year)')
+                scrape(queries)
+            except:
+                pdp_list.append('Error')
 
-prices = pd.DataFrame(np.concatenate((sd_vod, hd_vod, sd_est, hd_est), axis=1), 
-                  columns = ['AMZN SD VOD', 'AMZN HD VOD', 
-                             'AMZN SD EST', 'AMZN HD EST'])
+    prices = pd.DataFrame(np.concatenate((sd_vod, hd_vod, sd_est, hd_est), axis=1), 
+                      columns = ['AMZN SD VOD', 'AMZN HD VOD', 
+                                 'AMZN SD EST', 'AMZN HD EST'])
+    df_dict[territory] = pd.concat([df,prices,pd.DataFrame(pdp_list,columns=['URL'])], axis=1).replace([0], [None])
     
 ######################################################################
 
 ########################## SORT AND EXPORT ###########################
 
-df_dict[territory] = pd.concat([df,prices,pd.DataFrame(pdp_list,columns=['URL'])], axis=1).replace([0], [None])
 end = time.time()
 print('Scrape finished in',round((end-start)/60,2),'minutes.')
 print('Exporting file to enclosing folder...')
