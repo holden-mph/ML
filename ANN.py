e3d92ff2dc920ff18c5c13b8fe67977fc03d1316
holden-mph <holden.mph@gmail.com>
2018-04-15 20:56:53 +0100

    Update ANN.py

--- ANN.py
+++ ANN.py
@@ -2,9 +2,6 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 
-import matplotlib.pyplot as plt
-%matplotlib inline
-
 Nclass = 500
 
 X1 = np.random.randn(Nclass,2) + np.array([-2,2])
