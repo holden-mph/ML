fd4411edafb1493e73f5200c5e6eddbc3377685a
holden-mph <holden.mph@gmail.com>
2018-04-15 20:56:29 +0100

    Update ANN.py

--- ANN.py
+++ ANN.py
@@ -5,8 +5,6 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 %matplotlib inline
 
-from sklearn.model_selection import train_test_split
-
 Nclass = 500
 
 X1 = np.random.randn(Nclass,2) + np.array([-2,2])
